c9cb376de881bad15e4d20345dc228bd8e980f24
n0mn0mn0m <jakeheaden@gmail.com>
2018-09-01 20:42:15 +0100

    Update ShittyScanner.py

--- ShittyScanner.py
+++ ShittyScanner.py
@@ -9,10 +9,13 @@ class Scanner:
         self.content = self.readFile(fileName)
         self.signatures = [
             # Just keep adding signatures here to expand the search list
-            {'name' : 'JPEG', 'header' : b'\xff\xd8\xff', 'trailer' : b'\xff\xd9', 'extension' : '.jpg'},
-            {'name' : 'PNG', 'header' : b'\x89\x50\x4E\x47\x0D\x0A\x1A\x0A', 'trailer' : b'\x49\x45\x4E\x44\xAE\x42\x60\x82', 'extension' : '.png'},
-            {'name' : 'MPEG2', 'header' : b'\x00\x00\x01\xBA', 'trailer' : '\x00\x00\x01\xB9', 'extension' : '.mpg'},
-            {'name' : 'MPEG', 'header' : b'\x00\x00\x01', 'trailer' : '\x00\x00\x01\xB7', 'extension' : '.mpg'}
+            # type 0 == has header and trailer, 1 == size is in header
+            # for type 1, header should be the first part of the header, trailer should be the part
+            # that comes after the size byte(s)
+            {'type' : '0', 'name' : 'JPEG', 'header' : b'\xff\xd8\xff', 'trailer' : b'\xff\xd9', 'extension' : '.jpg'},
+            {'type' : '0', 'name' : 'PNG', 'header' : b'\x89\x50\x4E\x47\x0D\x0A\x1A\x0A', 'trailer' : b'\x49\x45\x4E\x44\xAE\x42\x60\x82', 'extension' : '.png'},
+            {'type' : '0', 'name' : 'MPEG2', 'header' : b'\x00\x00\x01\xBA', 'trailer' : '\x00\x00\x01\xB9', 'extension' : '.mpg'},
+            {'type' : '0', 'name' : 'MPEG', 'header' : b'\x00\x00\x01', 'trailer' : '\x00\x00\x01\xB7', 'extension' : '.mpg'}
 
         ]
     
@@ -30,10 +33,14 @@ class Scanner:
                 start = self.content.find(signature['header'], start + 1)
                 if start > 0:
                     end = self.content.find(signature['trailer'])
-                    if end > 0
-                    print(signature['name'] + ' Found')
-                    if extract == True:
-                        self.extractFile(start, end, signature['extension'], signature['name'])
+                    if end > 0:
+                        if signature['type'] == '1':  
+                            end = int.from_bytes(self.content[start + len(signature['header']):end))
+                            
+                        print(signature['name'] + ' Found')
+                        if extract == True:
+                            self.extractFile(start, end, signature['extension'], signature['name']
+                                
                 else:
                     break
     
